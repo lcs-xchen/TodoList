6ed186d287869a9cf97621dfe884f4d9e78cb261
Cindy Chen <xchen@lcs.on.ca>
2024-04-05 09:24:10 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Cindy Chen on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
