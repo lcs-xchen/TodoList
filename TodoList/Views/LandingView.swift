436880d9d87edaf88c960d67489e31a63ceae012
Cindy Chen <xchen@lcs.on.ca>
2024-04-08 14:10:33 -0400

    Added the search field and the new to-do item entry area

---
+++ TodoList/Views/LandingView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Cindy Chen on 2024-04-05.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    
+    // MARK: Stored properties
+    
+    // The item currently being added
+    @State var newItemDescription: String = ""
+    
+    //The list of items the user has added so far
+    @State var itemsAdded: [String] = []
+    
+    // The search text
+    @State var searchText = ""
+    
+    // MARK: Computed properties
+    var body: some View{
+        NavigationView{
+            
+            VStack {
+                
+                List {
+                    
+                    Text("Study for Chemistry quiz")
+                    Text("Finish Computer Science assignment")
+                    Text("Go for a run around campus")
+                   
+                }
+                .searchable(text: $searchText)
+                
+                
+                HStack {
+                    TextField("Enter a to-do item", text: $newItemDescription)
+                    
+                    
+                    Button("ADD") {
+                        // Add the new to-do item
+                    }
+                    .font(.caption)
+                }
+                .padding(20)
+            }
+            .navigationBarTitle("To do")
+        }
+    }
+    
+    // MARK: Functions
+    func add(){
+        //save user idea
+        itemsAdded.append(newItemDescription)
+    }
+}
+
+    
+#Preview {
+    LandingView()
+}
